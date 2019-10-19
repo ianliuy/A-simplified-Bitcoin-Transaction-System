41fe17bfed79a5fac19fe575bb4cb4e94cfabd38
sakila <i@yiyangliu.me>
2019-10-19 13:15:56 +0800

    setHash

--- main.go
+++ main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+// 0. 定义结构
+type Block struct {
+	// 1. 前区块哈希
+	PrevHash []byte
+	// 2. 当前区块哈希
+	Hash []byte
+	// 3. 数据
+	Data []byte
+}
+
+// 2. 创建区块
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := Block{
+		PrevHash: prevBlockHash,
+		Hash:     []byte{}, //先空 //TODO
+		Data:     []byte(data),
+	}
+	block.SetHash()
+	return &block
+}
+func (block *Block) SetHash() {
+	// 1. 拼装数据
+	blockInfo := append(block.PrevHash, block.Data...)
+	// 2. sha256
+	hash := sha256.Sum256(blockInfo)
+	block.Hash = hash[:]
+}
+
+func main() {
+	block := NewBlock("老师转", []byte{})
+	fmt.Printf("前区块哈希： %x\n", block.PrevHash)
+	fmt.Printf("当前区块哈希： %x\n", block.Hash)
+	fmt.Printf("区块数据： %s\n", block.Data)
+
+}
