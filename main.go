7c3b57f51cf98c7ed2eb2be8b5a88becd756f728
sakila <i@yiyangliu.me>
2019-10-19 13:29:40 +0800

    build genesisBlock

--- main.go
+++ main.go
@@ -25,6 +25,8 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 	block.SetHash()
 	return &block
 }
+
+// 3. 生成哈希
 func (block *Block) SetHash() {
 	// 1. 拼装数据
 	blockInfo := append(block.PrevHash, block.Data...)
@@ -33,10 +35,36 @@ func (block *Block) SetHash() {
 	block.Hash = hash[:]
 }
 
+// 4. 引入区块
+type BlockChain struct {
+	// 定义一个区块链数组
+	blocks []*Block
+}
+
+// 5. 定义一个区块链
+func NewBlockChain() *BlockChain {
+	// 创建一个创世块并作为第一个区块添加到区块链中
+	genesisBlock := GenesisBlock()
+	return &BlockChain{
+		blocks: []*Block{genesisBlock},
+	}
+}
+
+// 创世块
+func GenesisBlock() *Block {
+	return NewBlock("创世块", []byte{})
+}
+
+// 5. 添加区块
+// 6. 重构代码
+
 func main() {
-	block := NewBlock("老师转", []byte{})
-	fmt.Printf("前区块哈希： %x\n", block.PrevHash)
-	fmt.Printf("当前区块哈希： %x\n", block.Hash)
-	fmt.Printf("区块数据： %s\n", block.Data)
+	bc := NewBlockChain()
+	for i, block := range bc.blocks {
+		fmt.Printf("===========当前区块高度： % ==============\n", i)
+		fmt.Printf("前区块哈希： %x\n", block.PrevHash)
+		fmt.Printf("当前区块哈希： %x\n", block.Hash)
+		fmt.Printf("区块数据： %s\n", block.Data)
+	}
 
 }
