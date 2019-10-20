1ff647401025ede537a75bd4306178b7d4d062d1
sakila <i@yiyangliu.me>
2019-10-21 00:47:10 +0800

    modify main(use interator)

--- blockchainiterator.go
+++ blockchainiterator.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/boltdb/bolt"
+import (
+	"github.com/boltdb/bolt"
+	"log"
+)
 
 type BlockChainInterator struct {
 	db *bolt.DB
@@ -12,6 +15,27 @@ func (bc *BlockChain) NewIterator() *BlockChainInterator {
 	return &BlockChainInterator{
 		db: bc.db,
 		// 最初指向区块的最后一个区块
+		// tail []byte // value of last block's hash
 		currentHashPointer: bc.tail,
 	}
+}
+
+// 迭代器是属于区块链的，Next是迭代器的
+// 1. 返回当前区块
+// 2. 指针前移
+func (it *BlockChainInterator) Next() *Block {
+	var block Block
+	// func (db *DB) View(fn func(*Tx) error) error {
+	it.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(blockBucket))
+		if bucket == nil {
+			log.Panic("errors occur when iterator working")
+		}
+		// 解码
+		blockTmp := bucket.Get(it.currentHashPointer)
+		block = Deserialize(blockTmp)
+		it.currentHashPointer = block.PrevHash
+		return nil
+	})
+	return &block
 }
--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
+import "fmt"
+
 // 6. 重构代码
 
 func main() {
 	bc := NewBlockChain()
 	bc.AddBlock("first transaction")
 	bc.AddBlock("second transaction")
-	// 迭代器
-	/*for i, block := range bc.blocks {
-		fmt.Printf("===========当前区块高度： %d ==============\n", i)
+
+	// 调用迭代器 返回每一个数据
+	it := bc.NewIterator()
+	for {
+		block := it.Next()
+		fmt.Printf("===========当前区块高度：  ==============\n")
 		fmt.Printf("前区块哈希： %x\n", block.PrevHash)
 		fmt.Printf("当前区块哈希： %x\n", block.Hash)
 		fmt.Printf("区块数据： %s\n", block.Data)
-	}*/
+		if len(block.PrevHash) == 0 {
+			fmt.Printf("over")
+			break
+		}
+	}
 }
