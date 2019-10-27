3c53d9795524fc12bae83b06ff0053f89b397180
sakila <i@yiyangliu.me>
2019-10-27 16:08:56 +0800

    add NewCoinbaseTransaction

--- transaction.go
+++ transaction.go
@@ -7,6 +7,8 @@ import (
 	"log"
 )
 
+const reward = 12.5
+
 // 1. 定义交易结构
 type Transaction struct {
 	TXID      []byte     // 交易ID
@@ -45,7 +47,19 @@ func (tx *Transaction) SetHash() {
 	tx.TXID = hash[:]
 }
 
-// 2. 提供创建交易的方法
+// 2. 提供创建交易的方法(挖矿交易 coinbase)
+func NewCoinbaseTX(address string, data string) *Transaction {
+	// 挖矿交易的特点：只有一个input
+	// 无需引用交易id
+	// 无需引用index
+	// 矿工挖矿时无需指定签名，因此sig可以自由填写。一般填写矿工（矿池）的名字
+	input := TXInput{[]byte{}, -1, data}
+	output := TXOutput{reward, address}
+	tx := Transaction{[]byte{}, []TXInput{input}, []TXOutput{output}}
+	tx.SetHash()
+	return &tx
+	// Transaction{}
+}
 
 // 3. 创建挖矿交易
 
