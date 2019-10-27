6047fd786179b7bb99c99c7055b648cae9446cec
sakila <i@yiyangliu.me>
2019-10-27 15:22:04 +0800

    add SetHash

--- transaction.go
+++ transaction.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"log"
+)
+
 // 1. 定义交易结构
 type Transaction struct {
 	TXID      []byte     // 交易ID
@@ -22,6 +29,20 @@ type TXOutput struct {
 	value float64
 	// 锁定脚本，用地址模拟
 	PubKeyHash string
+}
+
+// 设置交易ID
+
+func (tx *Transaction) SetHash() {
+	var buffer bytes.Buffer
+	encoder := gob.NewEncoder(&buffer)
+	err := encoder.Encode(tx)
+	if err != nil {
+		log.Panic(err)
+	}
+	data := buffer.Bytes()
+	hash := sha256.Sum256(data)
+	tx.TXID = hash[:]
 }
 
 // 2. 提供创建交易的方法
