d643a233ac3a96497894ef789efe074a2f773ac6
sakila <i@yiyangliu.me>
2019-10-28 14:21:38 +0800

    add command "send FROM TO AMOUNT MINER DATA"

--- cli.go
+++ cli.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 // 用来接收命令行参数并且控制区块链操作的文件
@@ -15,6 +16,7 @@ const Usage = `
 	addBlock      --data     DATA          "add data to blockchain"
 	printChain                             "print all blockchain data"
     getBalance    --address  ADDRESS       "获取指定地址的余额"
+    send FROM TO AMOUNT MINER DATA         "由FROM转AMOUNT给TO，由MINER挖矿，同时写入DATA"
 `
 
 // 接受参数的动作放到一个函数中
@@ -67,6 +69,22 @@ func (cli *CLI) Run() {
 			address := args[3]
 			cli.GetBalance(address)
 		}
+	case "send":
+		fmt.Printf("start sending...\n")
+		if len(args) != 7 {
+			fmt.Printf("参数个数错误\n")
+			fmt.Printf(Usage)
+			return
+		}
+		// send FROM TO AMOUNT MINER DATA         "由FROM转AMOUNT给TO，由MINER挖矿，同时写入DATA"
+
+		from := args[2]
+		to := args[3]
+		// func ParseFloat(s string, bitSize int) (float64, error) {
+		amount, _ := strconv.ParseFloat(args[4], 64)
+		miner := args[5]
+		data := args[6]
+		cli.Send(from, to, amount, miner, data)
 
 	default:
 		// fmt.Printf("Invalid command")
@@ -75,4 +93,12 @@ func (cli *CLI) Run() {
 	// 分析命令
 	// 1. 添加区块 2. 打印区块
 	// 执行相应动作
+}
+func (cli *CLI) Send(from, to string, amount float64, miner, data string) {
+	fmt.Printf("from: %s\n", from)
+	fmt.Printf("to: %s\n", to)
+	fmt.Printf("amount:%f\n", amount)
+	fmt.Printf("miner: %s\n", miner)
+	fmt.Printf("data: %s\n", data)
+	// 具体的逻辑 TODO
 }
--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@ rm *.db
 go run *.go
 go run *.go printChain
 go run *.go getBalance --address genesis
+go run *.go send genesis aaa 10 minerbbb "1st transaction"
